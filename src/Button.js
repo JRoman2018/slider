d4001cbb4e72318d4bba399f076b8c365e88b908
Jose <manitogames24@gmail.com>
2020-12-15 16:26:28 -0400

    slider app

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+ import React from 'react';
+ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+
+const Button = ({setIndex, index}) =>{
+    return (
+        <>
+            <button className="prev" onClick={() => setIndex(index - 1)}>
+                <FiChevronLeft/>
+            </button>
+            <button className="next" onClick={() => setIndex(index + 1)}>
+                <FiChevronRight/>
+            </button>
+        </>
+    )
+
+}
+
+export default Button
